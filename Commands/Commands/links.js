d72e9bb785b267a6d3960bfd10c9ce999c798b86
IchiiSama <45918948+IchiiSama@users.noreply.github.com>
2019-08-10 16:54:27 +0200

    Update links.js

--- Commands/Commands/links.js
+++ Commands/Commands/links.js
@@ -12,8 +12,8 @@ class links {
             .attachFile("assets/yourimage.png") //your banner image, delete this line and the next line if you will not use it
             .setImage("attachment://yourimage.png")
             .addField(`Inviter le bot:`, `[Lien d'invitation](${invite_link})`)
-            .addField(`Rejoindre le serveur Support:`, `[ImguBot - Support](https://gg.discord.fr)`)
-            .addField(`Voter pour le bot`, `[Aller sur discordbots](https://discordbots.org/bot/576104086479568916)`)
+            .addField(`Rejoindre le serveur Support:`, `[ImguBot - Support](https://discord.gg/cUfCJZ)`) //put your own support server here
+            .addField(`Voter pour le bot`, `[Aller sur discordbots](discordbot_link)`) //put your link to DiscordBots.org here
             .setFooter(`links - Awaken by ${message.member.user.tag}`)
             .setTimestamp()
         message.channel.send(embed)
